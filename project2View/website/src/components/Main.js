138ebf0440ebcb85a8b05cca018f0490a2af4555
Carl <carlshefcik834@gmail.com>
2019-11-25 16:22:48 -0800

    User navigation to interviewscreen established

--- project2View/website/src/components/Main.js
+++ project2View/website/src/components/Main.js
@@ -1,10 +1,53 @@
 import React, { Component } from 'react'
+import { Link } from "react-router-dom";
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+
+
+const styles = {
+    button: {
+      margin: "15px 5px 15px 5px",
+    },
+}
+
+export function CenteredGrid() {
+  
+    return (
+        <div>
+            <Grid container direction="row" justify="center" alignItems="center" spacing={3}>
+                <Grid item xs>
+                    <div style={styles.button}>
+                        <Button variant="contained" disabled><h2>Patients</h2></Button>
+                    </div>
+                </Grid>
+                <Grid item xs>
+                    <div style={styles.button}>
+                        <Link to="/Visits"><Button variant="contained"><h2>Visits</h2></Button></Link>
+                    </div>
+                </Grid>
+                <Grid item xs>
+                    <div style={styles.button}>
+                        <Button variant="contained" disabled><h2>Analytics</h2></Button>
+                    </div>
+                </Grid>
+                <Grid item xs>
+                    <div style={styles.button}>
+                        <Button variant="contained" disabled><h2>Other</h2></Button>
+                    </div>
+                </Grid>
+            </Grid>
+        </div>
+    ); 
+}
 
 export default class Main extends Component {
+
     render() {
+
         return (
             <div>
                 <h1>Home Page</h1>
+                { CenteredGrid() }
             </div>
         )
     }
